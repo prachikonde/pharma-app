ab67f05f34f7e6f943835f6a67a6d63f4943224d
PrachiKonde <PrachiKonde@WAILAP147>
2024-06-20 23:08:13 +0530

    functon changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-const Navbar = () => {
+export default function Navbar() {
   return (
-    
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
+   <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
     <Link className="navbar-brand" to="/">Pharma App</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -35,4 +34,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+//export default Navbar
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React , { useState } from 'react'
 
-const ProductList = () => {
+export default function  ProductList()
+ {
+  const [products] = useState([
+    {
+    id: 101,
+    prdname: "Bottle",
+    price: 500,
+    image: "./images/pharma/bottle.jpg"
+  },
+  {
+    id: 102,
+    prdname: "Tralley",
+    price: 340,
+    image: "./images/pharma/tralley.jpg"
+  },
+  {
+    id: 103,
+    prdname: "BP Reader",
+    price: 190,
+    image: "./images/pharma/readingmachine.jfif"
+  },
+  {
+    id: 104,
+    prdname: "OIP",
+    price: 400,
+    image: "./images/pharma/OIP.jfif"
+  }
+]);
   return (
     <div>
-      Product List goes here
+      <h1>Product List</h1>
+      <div className="container">
+        <div className="row">
+          {products && products.map((product) => (
+            <div className="col-md-3">
+              <div className="card">
+                <img src={product.image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{product.prdname}</h5>
+                  <p className="card-text">Price: {product.price}</p>
+                  <a href="/" className="btn btn-primary">Add to Cart</a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
 
-export default ProductList
+//export default ProductList
